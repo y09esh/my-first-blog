94de12defb79bc796450c876c67ead668b8ecf1e
plasma <yogeshkumar02691@gmail.com>
2020-03-06 22:13:45 +0530

    modified blog/models.py,teplates/blog/post_list.html and views.py

--- blog/models.py
+++ blog/models.py
@@ -1,13 +1,15 @@
-import django
-class Post(django.db.models.Model):
+from django.db import models
+from django.conf import settings
+from django.utils import timezone
+class Post(models.Model):
     """docstring for Posts."""
-    auther = django.db.models.ForeignKey(django.conf.settings.AUTH_USER_MODEL,on_delete=django.db.models.CASCADE)
-    title = django.db.models.CharField(max_length = 200)
-    text = django.db.models.TextField()
-    created_date = django.db.models.DateTimeField(default = django.utils.timezone.now())
-    published_date = django.db.models.DateTimeField(blank = True, null = True)
+    auther = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now())
+    published_date = models.DateTimeField(blank = True, null = True)
     def publish(self):
-        self.published_date = django.utils.timezone.now()
+        self.published_date = timezone.now()
         self.save()
     def __str__(self):
         return self.title
