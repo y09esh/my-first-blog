e13ed7fcf3539046f652079130014a820c68ccd1
plasma <yogeshkumar02691@gmail.com>
2020-03-11 16:06:52 +0530

    new changes by using django 2 by example

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,44 @@
 from django.db import models
 from django.conf import settings
 from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset() \
+                     .filter(status='published')
+               
 class Post(models.Model):
     """docstring for Posts."""
-    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE)
+    STATUS_CHOICES = (
+        ('drafy','Draft'),
+        ('published','Published'),
+        )
+    
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     title = models.CharField(max_length = 200)
+    slug = models.SlugField(max_length = 200,
+                            unique_for_date='publish',
+                            null='Null')
     text = models.TextField()
-    created_date = models.DateTimeField(default = timezone.now())
-    published_date = models.DateTimeField(blank = True, null = True)
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    publish = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10,
+                              choices=STATUS_CHOICES,
+                              default='draft')
+    objects=models.Manager() #the default manager
+    published = PublishedManager() # Our custom manager.
+    class Meta:
+        ordering = ('-publish',)
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail',
+                       args=[self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug])
--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+app_name='blog'
 
 urlpatterns= [
-    path('',views.post_list,name='post_list'),
-    path('post/<int:pk>/', views.post_detail, name = 'post_detail'),
-    path('post/new/',views.post_new, name='post_new'),
-    path('post/<int:pk>/edit/', views.post_edit, name = 'post_edit'),
+    # post views
+    #path('',views.post_list,name='post_list'),
+    path('',views.PostListView.as_view(),name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+         views.post_detail,
+         name = 'post_detail'),
+    #path('post/new/',views.post_new, name='post_new'),
+    #path('post/<int:pk>/edit/', views.post_edit, name = 'post_edit'),
     ]
